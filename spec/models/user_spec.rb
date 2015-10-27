dc285e1c923347cb14b7be22c9e577b8214e57b6
Tyler Shipe <tjshipe@gmail.com>
2015-10-26 22:20:31 -0400

    first user specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryGirl.create(:user) }
+
+  subject { user }
+
+  it { should respond_to(:posts) }
 end
