3e4f6b38b8675dbdc276d1d1cea4dd439fae838c
Tyler Shipe <tjshipe@gmail.com>
2015-10-27 19:16:56 -0400

    create relationship model and spec files

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-         
+
   has_many :posts
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followers, through: :relationships
 end
