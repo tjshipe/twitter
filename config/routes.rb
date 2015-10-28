21ecd4c44f84bcfe81ce38cc8b6c0e3db083005b
Tyler Shipe <tjshipe@gmail.com>
2015-10-28 00:34:44 -0400

    add relationships controller, routes, and spec file

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :posts, except: [:updated, :edit, :show]
   resources :users, except: [:new, :destroy]
+  resources :relationships, only: [:create, :destroy]
 
   root 'posts#index'
 end
