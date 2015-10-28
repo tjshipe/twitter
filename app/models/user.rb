72fb8480c9dd5b18dd1ae68f2980abb2b11f0bff
Tyler Shipe <tjshipe@gmail.com>
2015-10-28 00:32:43 -0400

    create followed_user and followers association on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,10 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :posts
-  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
-  has_many :followers, through: :relationships
+
+  has_many :following_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through: :following_relationships, source: :followed
+
+  has_many :follower_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :follower_relationships, source: :follower
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,6 @@ RSpec.describe User, type: :model do
 
   it { should respond_to(:posts) }
   it { should respond_to(:email) }
+  it { should respond_to(:followers) }
+  it { should respond_to(:followed_users) }
 end
