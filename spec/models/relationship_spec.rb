69409c8354566299400367fd59d386d44df3ddd2
Tyler Shipe <tjshipe@gmail.com>
2015-10-28 00:27:56 -0400

    add relationship validation and specs

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Relationship, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:relationship) { FactoryGirl.create(:relationship) }
+
+  subject { relationship }
+
+  it { should respond_to(:follower_id) }
+  it { should respond_to(:followed_id) }
+
+  context "validations" do
+    it{ should validate_presence_of(:follower_id)       }
+    it{ should validate_presence_of(:followed_id) }
+  end
 end
