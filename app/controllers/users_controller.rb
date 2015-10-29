a9157d177bcc5589892d3d47bcdef73126452cc6
Tyler Shipe <tjshipe@gmail.com>
2015-10-29 10:57:07 -0400

    remove unused code from users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,12 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_user, only: [:show, :edit, :update]
 
   def index
     @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
     @posts = @user.posts
   end
-
-  private
-
-    def set_user
-      @user = User.find(params[:id])
-    end
-
-
-    def user_params
-      params.require(:user).permit(:email)
-    end
 end
