0bf488459227554b15ae966388bfacad35fe11d0
Tyler Shipe <tjshipe@gmail.com>
2015-10-29 10:02:24 -0400

    add relationships spec

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,12 +4,15 @@ class RelationshipsController < ApplicationController
   def create
     @user = User.find(params[:relationship][:followed_id])
     current_user.follow!(@user)
-    redirect_to @user
+    redirect_to users_path
   end
 
   def destroy
-    @user = Relationship.find(params[:id]).followed
-    current_user.unfollow!(@user)
-    redirect_to @user
+    if @user = Relationship.find(params[:id]).followed
+      current_user.unfollow!(@user)
+      redirect_to users_path
+    else
+      redirect_to users_path, notice: 'Not able to unfollow.'
+    end
   end
 end
